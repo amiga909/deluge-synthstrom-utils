9e3966a395f74ff795aaa7cac329981e68f020de
Roman Hatz <romanhatz@Romans-iMac.zyxel.com>
2018-01-21 18:11:21 +0100

    format

--- archive_unused_recordings.rb
+++ archive_unused_recordings.rb
@@ -1,103 +1,93 @@
 
 class Cleaner
-#!/usr/bin/env ruby
+	#!/usr/bin/env ruby
 
-require 'fileutils'
-
-
-
-@@usedRecs = []
-@@unusedRecs = []
-@@recs = []
-
- def initialize(isDry)
- 	args = ARGV.join(" ")
-@isDryRun = args.match("dry") != nil ? true : false 	
-@isEnableDebug = args.match("debug") != nil || false 
-@includeRecord = args.match("exclude-record") != nil ? false : true 
-@includeResample = args.match("exclude-resample") != nil ? false : true
-@includeUserSamples = args.match("include-user") != nil ? true : false
+	require 'fileutils'
 
 
-class << self
-    attr_accessor :isDryRun
-  end 	
 
-  end
+	@@usedRecs = []
+	@@unusedRecs = []
+	@@recs = []
 
-  
+	def initialize(isDry)
+		args = ARGV.join(" ")
+		@isDryRun = args.match("dry") != nil ? true : false 	
+		@isEnableDebug = args.match("debug") != nil || false 
+		@includeRecord = args.match("exclude-record") != nil ? false : true 
+		@includeResample = args.match("exclude-resample") != nil ? false : true
+		@includeUserSamples = args.match("include-user") != nil ? true : false
 
-def getFiles
 
-Dir[ "./SAMPLES/RECORD/*.{WAV,wav}"].each do |f|
+		class << self
+			attr_accessor :isDryRun
+		end 	
+
+	end
+
 	
-    @@recs.push(f.sub('./SAMPLES', 'SAMPLES')) unless File.directory? f
-  end
-  Dir[ "./SAMPLES/RESAMPLE/*.{WAV,wav}"].each do |f|
+	def getFiles
 
-    @@recs.push(f.sub('./SAMPLES', 'SAMPLES')) unless File.directory? f
-  end
- 
-Dir[ "./*/*.{XML,xml}"].each do |f|
-    parseFile(f) unless File.directory? f
-  end
-   
-    @@usedRecs.uniq!
-@@recs.uniq!
- @@unusedRecs = @@recs -  @@usedRecs
-    
-end
+		Dir[ "./SAMPLES/RECORD/*.{WAV,wav}"].each do |f|
+						@@recs.push(f.sub('./SAMPLES', 'SAMPLES')) unless File.directory? f
+		end
+		Dir[ "./SAMPLES/RESAMPLE/*.{WAV,wav}"].each do |f|
 
-def display
-	p "To Archive: #{@@unusedRecs }"
-	p %{ recs: #{@@recs.size} | 
+			@@recs.push(f.sub('./SAMPLES', 'SAMPLES')) unless File.directory? f
+		end
+				Dir[ "./*/*.{XML,xml}"].each do |f|
+			parseFile(f) unless File.directory? f
+		end
+				@@usedRecs.uniq!
+		@@recs.uniq!
+		@@unusedRecs = @@recs -  @@usedRecs
+			end
+
+	def display
+		p "To Archive: #{@@unusedRecs }"
+		p %{ recs: #{@@recs.size} | 
 usedRecs: #{@@usedRecs.size} |
 unusedRecs: #{@@unusedRecs.size}
  	}.gsub(/\s+/, " ").strip
-    
-end 
+			end 
 
-def archive 
-	cnt = 0
-	@@unusedRecs.each do |f|
-		f = './'+f
-		if File.exist?(f)
-			FileUtils::mkdir_p './_ARCHIVED/'+f
-			cnt = cnt + 1
-		  FileUtils.mv(f, './_ARCHIVED/'+f) 
-		end 	
-	end 
-	p "Done. Archived #{cnt} Files."
-end	
+	def archive 
+		cnt = 0
+		@@unusedRecs.each do |f|
+			f = './'+f
+			if File.exist?(f)
+				FileUtils::mkdir_p './_ARCHIVED/'+f
+				cnt = cnt + 1
+				FileUtils.mv(f, './_ARCHIVED/'+f) 
+			end 	
+		end 
+		p "Done. Archived #{cnt} Files."
+	end	
 
-def parseFile path
-	File.foreach(path) do |f|
-		match = f.match(/(SAMPLES\/RECORD\/REC[0-9]+\.WAV)/)
-		@@usedRecs.push(match[0]) if match
-    	
-    	match = f.match(/(SAMPLES\/RESAMPLE\/REC[0-9]+\.WAV)/)
-		@@usedRecs.push(match[0]) if match
-    end
+	def parseFile path
+		File.foreach(path) do |f|
+			match = f.match(/(SAMPLES\/RECORD\/REC[0-9]+\.WAV)/)
+			@@usedRecs.push(match[0]) if match
+						match = f.match(/(SAMPLES\/RESAMPLE\/REC[0-9]+\.WAV)/)
+			@@usedRecs.push(match[0]) if match
+		end
 
-end
+	end
 
 end
-		
-  	
 c = Cleaner.new(true)
 
 p c 
 
 
 if c.isDryRun()
-c.getFiles()
-c.display()
-   	
- 	else 
- 		c.getFiles()
-c.display()
-c.archive() 
- 	end
+	c.getFiles()
+	c.display()
+	else 
+	c.getFiles()
+	c.display()
+	c.archive() 
+end
 
 
 
