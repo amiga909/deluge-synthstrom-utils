5cf1324313181239642a648e8bf249b33a9f8926
Roman Hatz <lfo@gmx.ch>
2018-07-14 21:46:15 +0200

    update

--- audioconvert.sh
+++ audioconvert.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 ####
-# Converts non 44hz .wav to 16bit 44hz
+# Convert all WAV files with a sample rate lower than 44hz to 44hz/16bit 
 ####
 
 
@@ -9,6 +9,14 @@
 IFS='
 '
 set -f
+
+
+minSampleRate=44100
+totalIncrease=0
+count=0
+convCount=0
+skippedCount=0
+
 
 if ! [ -x "$(command -v afinfo)" ]; then
 	echo 'Error: afinfo is not installed.'  
@@ -19,37 +27,64 @@ if ! [ -x "$(command -v afconvert)" ]; then
   	exit 1
 fi
 
- 
-if [ "$1" != "no-debug" ] ; then 
-	echo '-----------------TEST MODE-----------------'
-	echo "If the listing looks good, run normal mode: sh audioconvert.sh no-debug"
+if [ "$1" != "no-debug" ]; then 
+	echo '-----------------TEST_MODE-----------------'
+	echo "Simulation mode. For normal mode: sh audioconvert.sh no-debug"
 else 
-	echo '----------------NORMAL MODE----------------'	
+	echo '----------------NORMAL_MODE----------------'	
 fi
 
 
-for f in $(find ./ -type f -iname '*.wav'); do
-	size=$(stat -f%z "$f") 
-	size=$(($size / 1024)) 
-	isInvalid=$(afinfo "$f" | grep -L "44100 Hz," )
-	
-	echo '------------------------'
-	echo "$f ($size KB)"
+if [ "$2" != "" ]; then 
+	echo "$2"
+fi
 
-	if ! [ "$isInvalid" = "" ]; then
-	 
-		#echo "$(afinfo $f)"
+ 
+for f in $(find ./ -type f -iname '*.wav'); do
+	count=$((count+1))
+
+	{  
+	    afInfo="$(afinfo "$f")"
+
+	} || {  
+		echo '------------------------'
+	 	echo "Error. Invalid file. Skip $f"
+		skippedCount=$((skippedCount+1))
+		continue 
+	}
+
+	
+	invalidWav="$(echo "$afInfo" | grep -o 'AudioFileOpenURL failed' )" 
+	sampleRate="$(echo "$afInfo" | grep -o 'Data format: .*' | grep -o '[0-9]\{3,7\} Hz' | grep -o '[0-9]\{3,7\}' )"
+	fishyMp3="$(echo "$afInfo" | grep -o 'Data format: .*' | grep "Hz, '.mp3' " )"
+
+	if [[ $sampleRate -lt $minSampleRate  || $fishyMp3 != "" ]]; then
+		size=$(stat -f%z "$f") 
+		sizeBefore=$(($size / 1024)) 
+
+		echo '------------------------'
+		echo "$f $sizeBefore KB, $sampleRate Hz)"
+ 
+		if [[ fishyMp3 != "" ]]; then
+			echo "Fishy MP3 Codec detected."
+		fi
+
 		if [ "$1" = "no-debug" ]; then
+			convCount=$((convCount+1))
 	    	afconvert -o "$f" -f 'WAVE' -d I16@44100 "$f"
 	  		size=$(stat -f%z "$f") 
-			size=$(($size / 1024)) 
-	  		echo "converted ($size KB)"
+			size=$(($size / 1024))
+			increase=$((size-sizeBefore))
+			totalIncrease=$((increase+totalIncrease))
+
+	  		echo "Converted: $size KB"
 	   else 
-	   		echo "would be converted: $f"
+	   		echo "Converted in normal mode only"
 	   fi
 	fi
-
     
 done
+
+echo "$convCount of $count files converted, $skippedCount skipped. Total disk space increase: $totalIncrease KB"
 
 exit 1
