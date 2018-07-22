f977e4141801c268c5c53ad85ac0ed52ce7b0ddd
Roman Hatz <lfo@gmx.ch>
2018-07-22 16:58:15 +0200

    ..

--- js/main.js
+++ js/main.js
@@ -2,31 +2,61 @@ const seekOrphans = require('./sampleOrphans');
 
 
 
-var isLoading = false;
-
-onload = function() {
-
-    $(document).ready(function() {
-        let $seekOff = $("#seek-off")
-        let $seekOn = $("#seek-on")
-
-        let $seekOffImg = $("#seek-off-img")
-        let $seekOnImg = $("#seek-on-img")
-
-
-        $seekOff.on("click", () => {
-            console.log("clicked jq")
-            $seekOff.hide()
-            $seekOn.show()
-        })
-        $seekOn.on("click", () => {
-            console.log("clicked jq")
-            $seekOn.hide()
-            $seekOff.show()
-        })
-    });
-
-
-
+const BTN_COLORS = {
+    'blank': 'assets/b_off.png',
+    'green': 'assets/b_green.png',
+    'blank_lumen': 'assets/b_off_lumen.png',
+    'pink': 'assets/b_pink.png',
+    'red': 'assets/b_red.png'
 
 }
+
+let $thaButton,
+    $thaButtonImg = null
+let isToggled = false
+let blinkerTimeout = 0
+
+function listen() {
+    $thaButton.on("mouseover", () => {
+        if (!isToggled) $thaButtonImg.attr("src", BTN_COLORS.blank_lumen)
+    })
+    $thaButton.on("mouseout", () => {
+        if (!isToggled) $thaButtonImg.attr("src", BTN_COLORS.blank)
+    })
+    $thaButton.on("click", () => {
+        $thaButtonImg.attr("src", BTN_COLORS.pink)
+
+        animateBtn()
+
+        if (isToggled) {
+            $thaButtonImg.attr("src", BTN_COLORS.red)
+            window.clearInterval(blinkerTimeout)
+            isToggled = false
+        } else { isToggled = true }
+
+    })
+}
+
+function init() {
+    $thaButton = $("#button-container")
+    $thaButtonImg = $("#button-container img")
+
+    listen()
+}
+
+function animateBtn() {
+    window.clearInterval(blinkerTimeout)
+    blinkerTimeout = window.setInterval(function() {
+        let $curr = $thaButtonImg.attr("src");
+        if ($curr == BTN_COLORS.blank) {
+            $thaButtonImg.attr("src", BTN_COLORS.pink)
+        } else {
+            $thaButtonImg.attr("src", BTN_COLORS.blank)
+        }
+    }, 500)
+}
+
+
+$(document).ready(function() {
+    init()
+});
