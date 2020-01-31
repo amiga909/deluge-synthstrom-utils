9513c36026b796866ed17e6049b5ce1b224c6ba3
amiga909 <lfo@gmx.ch>
2020-01-31 14:30:44 +0100

    update

--- multisampleslice.sh
+++ multisampleslice.sh
@@ -15,6 +15,8 @@ START_NOTE=12
 ARG1_working_dir="$1"
 WORKING_DIR="."
 cd $WORKING_DIR
+tempDir="$WORKING_DIR/temp"
+rm -rf "temp.wav" && rm -rf "$tempDir"
 
 NOTES=( )
 function note_table () {
@@ -42,27 +44,41 @@ for f in $(find "$WORKING_DIR" -type f -maxdepth 1 -iname '*.wav' | sort -h ); d
 			continue
 	fi
 	
-	tempDir="$WORKING_DIR/temp"
 	instrumentName=$(basename -- "$f")
 	instrumentName=${instrumentName/.wav/}
 	outputDir="$WORKING_DIR/$instrumentName"
-
-	length=$(soxi -D "$WORKING_DIR/$f")
-	sampleLength=$(bc -l <<< "$length/$NO_OF_NOTES")
 	 
 	rm -rf "$tempDir" && mkdir -p "$tempDir"
-	echo "Processing $f ($length sec)"
+	echo "Levelling $f ($file_size MB)..."
 
+	#https://thewhackhacker.wordpress.com/2018/09/26/companding-with-sox/
+	originalFilesDir="$WORKING_DIR/_originalFiles"
+	mkdir -p "$originalFilesDir"      
+	cp $f "$originalFilesDir/$f"        
+	#-91,-90,-10     .1,.1 -60,-10 0 0 .1    compand 0,.1 -70,-60,-20
+	sox -v 0.75 $f "temp.wav" compand .01,.3 -6,-4,-3,-3,0,-3 
+	sox --norm="-0.15" "temp.wav" "temp1.wav" 
+	mv temp1.wav $f
+	rm -f temp.wav
+
+	echo "Splitting ..."
+	#sox $f "$tempDir"/.wav trim 0 $sampleLength : newfile : restart
+	#sox "$f" "$tempDir"/.wav silence 1 0.5 1% 1 0.8 1% : newfile : restart
 	sox "$f" "$tempDir"/.wav silence 1 0.1 1% 1 0.8 1% : newfile : restart
 
-	for f in $(find "$tempDir" -type f -maxdepth 1 -iname '*.wav'  ); do
+	for f in $(find "$tempDir" -type f -maxdepth 1 -iname '*.wav' | sort ); do
 		file_size=$(stat -f%z "$f")
 		file_size=$((file_size + 0 ))
 		file_size=$((file_size / 1000  ))
 		if [ 10 -gt "$file_size" ]; then
 			rm -f "$f"
-			echo "Deleted $f | size $file_size KB"
+			#echo "Deleted $f | size $file_size KB"
+			continue
+
 		fi
+		sox -v 0.99 $f "temp.wav" fade 0.001 -0 0.001
+		mv temp.wav $f
+		
 	done
 
 	# name wav files if sample count is correct
@@ -76,18 +92,33 @@ for f in $(find "$WORKING_DIR" -type f -maxdepth 1 -iname '*.wav' | sort -h ); d
 		 	mv $f "$tempDir/$instrumentName.$cnt.${NOTES[$cnt]}.wav"	
 		 	cnt=$((cnt + 1))	
 		done
+
+		#@TODO remove octaves 
+		#for f in $(find "$tempDir" -type f -maxdepth 1 -iname '*.wav' | sort ); do
+		#done
+
 	else 
 		echo "!Error sample count. Expected: $NO_OF_NOTES Have: $tempDirCount"
 		outputDir="$outputDir-$tempDirCount"
 	fi
 
+	if [[ $instrumentName =~ "bass" ]]; then
+		echo "Convert $instrumentName to mono"
+	 	for f in $(find "$tempDir" -type f -maxdepth 1 -iname '*.wav' | sort ); do
+	 		#-v 0.99: sox WARN dither
+	 	    sox -v 0.99 "$f" -c 1 "temp.wav"
+	 		mv temp.wav $f
+	 	done
+	fi	
+	folderSize="$(du -hs $tempDir | cut -f1 | sed -e 's/ //g')"
+	outputDir="$outputDir--$folderSize"
+
 	rm -rf "$outputDir"
 	mv "$tempDir" "$outputDir"
+	
 
 	echo "Finished $instrumentName"
 	
-	
-
 
 done
 
