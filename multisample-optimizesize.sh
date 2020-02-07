7505f1784703d47860b34a5c3451aab41a0047c2
amiga909 <lfo@gmx.ch>
2020-02-07 10:10:58 +0100

    Update multisample-optimizesize.sh

--- multisample-optimizesize.sh
+++ multisample-optimizesize.sh
@@ -3,21 +3,18 @@
 ## Convert DAW multisample recordings to Deluge multisamples
 ## Helper
 ##
-
-
 WORKING_DIR="."
-
+MAX_INSTRUMENT_SIZE_MB=50
 tokenSkipLowerUpper="--skip12lowerupper"
 tokenSkipUpper="--skip12upper"
 tokenSkipLower="--skip12lower"
 octave=( c c# d d# e f f# g g# a a# h )
-
-echo "-----------------------------"
-folderSize="$(du -ch ./*/*.wav | grep total | cut -f1 | sed -e 's/ //g')"
-echo "All instruments size BEFORE: $folderSize"
  
 echo "-----------------------------"
 echo "Optimizing instrument size..."
+folderSizeBefore="$(du -ch ./*/*.wav | grep total | cut -f1 | sed -e 's/ //g')"
+
+# 1) Cut bad recordings (sometimes lowest and hightes octaves sound very bad)
 for instrument in $(find "$WORKING_DIR/" -type d -maxdepth 1 -iname '*--skip12*' | sort ); do
 	instName=$(basename -- "$instrument")
 	instNameRaw="${instName/-*/}"
@@ -46,27 +43,48 @@ for instrument in $(find "$WORKING_DIR/" -type d -maxdepth 1 -iname '*--skip12*'
 
 	else 
 		if [[ $instName =~ "--skip12lower" ]]; then
-			len=${#octave[@]}
-			for (( i=0; i<$len; i++ )); do 
-				addZero="0"
-				if [ "$i" -gt 9 ]; then
-					addZero=""
-				fi
-				rm -f "$WORKING_DIR"/"$instName"/"$addZero"0"$i".wav
-			done
+			for count in {1..12}; do
+				deletee="$(printf "%03d\n" "$count")"
+				rm -f "$WORKING_DIR"/"$instName"/"$deletee".wav
+			done 
 		fi
 	fi
+done
 
-	# limit octaves based on size
-	folderSize="$(du -hs $instrument | cut -f1 | sed -e 's/ //g' | sed -e 's/M//g')"
-	folderSize=$((folderSize + 0))
-	if [ "$folderSize" -gt 50 ]; then
-		#remove black keys
-		for count in {0..10}; do
-			rm -f "$WORKING_DIR"/"$instName"/*#*.wav
-		done
-	fi 
+
+# 2) Remove every 2nd sample until size limit met
+for instrument in $(find "$WORKING_DIR/" -type d -maxdepth 1 | sort ); do
 	
+	instName=$(basename -- "$instrument")
+	instNameRaw="${instName/-*/}"
+
+	if [ $instrument = './' ]; then
+	        # Will not run if no directories are available
+	        continue
+	fi  
+	folderSize="$(du -hs $instrument | cut -f1 | sed -e 's/ //g' | sed -e 's/M//g')"
+	# remove float, cast to int
+	folderSize="${folderSize/.*/}"
+	folderSize=$((folderSize + 0))
+	if [ "$MAX_INSTRUMENT_SIZE_MB" -gt  "$folderSize" ]; then
+		continue
+	fi
+	removeCounter=0
+	while [[ "$folderSize" -gt "$MAX_INSTRUMENT_SIZE_MB" && "10" -gt "$removeCounter" ]]; do
+		removeCounter=$((removeCounter + 1))
+		wavCount=0
+		for wav in $(find "$instrument" -type f -maxdepth 1 -iname '*.wav' | sort -h ); do
+			remainder=$(( wavCount % 2 ))
+			if (( $wavCount % 2 )); then
+				rm -f $wav
+			fi 
+			wavCount=$((wavCount + 1))
+		done
+		folderSize="$(du -hs $instrument | cut -f1 | sed -e 's/ //g' | sed -e 's/M//g')"
+		folderSize="${folderSize/.*/}"
+		folderSize=$((folderSize + 0))
+	done
+
 	folderSize="$(du -hs $instrument | cut -f1 | sed -e 's/ //g')"
 	fileCount="$(ls -1q "$instrument" | wc -l | sed -e 's/ //g')" 
 	outputDir="$instNameRaw-$fileCount--$folderSize"
@@ -74,4 +92,4 @@ for instrument in $(find "$WORKING_DIR/" -type d -maxdepth 1 -iname '*--skip12*'
 done
 
 folderSize="$(du -ch ./*/*.wav | grep total | cut -f1 | sed -e 's/ //g')"
-echo "All instruments size AFTER: $folderSize"
+echo "All instruments size after: $folderSize | before: $folderSizeBefore"
